671a7862813948e491df4a0610d9737ed15d0d24
Fazle Amin <163966633+FazleAmin-398@users.noreply.github.com>
2024-10-03 16:07:59 +0500

    Update question.sql

--- Restaurant Case Study/question.sql
+++ Restaurant Case Study/question.sql
@@ -1,1 +1,7 @@
-
+-- 1) Which product has the highest price? Only return a single row.
+-- 2)  Which customer has made the most orders?
+-- 3) What's the total revenue per product?
+-- 4) Find the orders and revenue trend
+-- 5) Find the first order (by date) for each customer.
+-- 6) Find the top 3 customers who have ordered the most distinct products
+-- 7) Which product has been bought the highest & least in terms of quantity?
